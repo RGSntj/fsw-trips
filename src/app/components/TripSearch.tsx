5d2e4cdd0a9503b4d3112fe4754951a0d6dd5a52
RGSntj <mariadefatima071965@gmail.com>
2023-07-05 14:45:52 -0300

    feat: add input currency and TripSearch components

--- src/app/components/TripSearch.tsx
+++ src/app/components/TripSearch.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/Button";
+import CurrencyInput from "@/components/CurrencyInput";
 import DatePicker from "@/components/DatePicker";
 
 import Input from "@/components/Input";
@@ -15,7 +16,12 @@ export function TripSearch() {
       <div className="flex flex-col gap-4 mt-5">
         <Input placeholder="Onde você quer ir?" />
         <div className="flex gap-4">
-          <DatePicker onChange={() => {}} placeholderText="Data de ida" />
+          <DatePicker
+            onChange={() => {}}
+            placeholderText="Data de ida"
+            className="w-full"
+          />
+          <CurrencyInput placeholder="Orçamento" />
         </div>
         <Button>Procurar</Button>
       </div>
