efa05c3c4925bee3012188987a1ba5bb91262be3
RGSntj <mariadefatima071965@gmail.com>
2023-07-11 23:00:57 -0300

    style: adapt do trip reservation desktop

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -91,7 +91,10 @@ export function TripReservation({
   const endDate = watch("endDate");
 
   return (
-    <div className="flex flex-col px-5">
+    <div className="flex flex-col px-5 lg:min-w-[380px] lg:p-5 lg:border-grayLighter lg:border lg:rounded-lg lg:shadow-md">
+      <p className="text-xl hidden text-primaryDarker mb-4 lg:block">
+        <span className="font-semibold">R${pricePerDay}</span> por dia
+      </p>
       <div className="flex gap-4">
         <Controller
           name="startDate"
@@ -160,7 +163,7 @@ export function TripReservation({
         </p>
       </div>
 
-      <div className="pb-10 border-b border-grayLighter w-full">
+      <div className="pb-10 border-b border-grayLighter w-full lg:border-none lg:pb-0">
         <Button
           onClick={() => handleSubmit(onSubmit)()}
           className="mt-4 w-full"
