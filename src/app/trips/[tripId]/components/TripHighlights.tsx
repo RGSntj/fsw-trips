ae320d4ff068859adffc52a3f0e6c51be6bb5e15
RGSntj <mariadefatima071965@gmail.com>
2023-07-11 23:59:38 -0300

    style: adapt trip details do desktop

--- src/app/trips/[tripId]/components/TripHighlights.tsx
+++ src/app/trips/[tripId]/components/TripHighlights.tsx
@@ -6,10 +6,12 @@ interface Props {
 
 export function TripHighlights({ highlights }: Props) {
   return (
-    <div className="flex flex-col p-5">
-      <h2 className="font-semibold text-primaryDarker mb-2">Destaques</h2>
+    <div className="flex flex-col p-5 lg:p-0">
+      <h2 className="font-semibold text-primaryDarker mb-2 lg:text-xl lg:mt-12">
+        Destaques
+      </h2>
 
-      <div className="flex flex-wrap gap-y-2">
+      <div className="flex flex-wrap gap-y-2 lg:mt-5">
         {highlights.map((highlight, index) => {
           return (
             <div key={highlight} className="flex items-center gap-3 w-1/2">
@@ -20,7 +22,9 @@ export function TripHighlights({ highlights }: Props) {
                 alt={highlight}
               />
 
-              <p className="text-grayPrimary text-xs">{highlight}</p>
+              <p className="text-grayPrimary text-xs lg:text-base">
+                {highlight}
+              </p>
             </div>
           );
         })}
--- src/app/trips/[tripId]/components/TripLocation.tsx
+++ src/app/trips/[tripId]/components/TripLocation.tsx
@@ -8,10 +8,12 @@ interface Props {
 
 export function TripLocation({ location, locationDescription }: Props) {
   return (
-    <div className="p-5">
-      <h2 className="font-semibold text-primaryDarker mb-5">Localização</h2>
+    <div className="p-5 lg:p-0 lg:mt-12 lg:pb-20">
+      <h2 className="font-semibold text-primaryDarker mb-5 lg:text-xl">
+        Localização
+      </h2>
 
-      <div className="relative h-[280px] w-full">
+      <div className="relative h-[280px] w-full lg:hidden">
         <Image
           src="/map-mobile.png"
           fill
@@ -21,11 +23,21 @@ export function TripLocation({ location, locationDescription }: Props) {
         />
       </div>
 
-      <h3 className="text-primaryDarker text-sm font-semibold mt-3">
+      <div className="relative h-[480px] hidden w-full lg:block">
+        <Image
+          src="/map-desktop.png"
+          fill
+          alt={location}
+          style={{ objectFit: "cover" }}
+          className="shadow-md rounded-lg"
+        />
+      </div>
+
+      <h3 className="text-primaryDarker text-sm font-semibold mt-3 lg:text-base lg:mt-5">
         {location}
       </h3>
 
-      <p className="text-xs leading-5 text-primaryDarker mt-3">
+      <p className="text-xs leading-5 text-primaryDarker mt-3 lg:text-sm lg:mt-4">
         {locationDescription}
       </p>
 
